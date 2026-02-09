a8ddafa2880a4ccb939afbbe3073980776746bd8
thosowe-creator <thosowe@gmail.com>
2026-02-09 14:04:00 +0900

    Add account page and Firebase sync

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  serverTimestamp,
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgmcfp1xHF9C-5705mfJDaujL3BaLkEs8",
+  authDomain: "cubetimerdb.firebaseapp.com",
+  projectId: "cubetimerdb",
+  storageBucket: "cubetimerdb.firebasestorage.app",
+  messagingSenderId: "1032993772532",
+  appId: "1:1032993772532:web:5d2953aca5fa06da779fa1",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+window.firebaseApp = app;
+window.firebaseAuth = auth;
+window.firebaseDb = db;
+window.firebaseAuthReady = new Promise((resolve) => {
+  onAuthStateChanged(auth, (user) => {
+    window.firebaseCurrentUser = user || null;
+    resolve(user || null);
+  });
+});
+
+window.firebaseAuthApi = {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+};
+window.firebaseDbApi = {
+  doc,
+  setDoc,
+  getDoc,
+  serverTimestamp,
+};
